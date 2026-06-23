9ad40d5a8db9a72263232a792857cd5dd087c320
VENKATESH C V <venkateshvairam@gmail.com>
2026-06-23 07:19:06 +0530

    Update: Lua line - filenaem with path

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -11,6 +11,23 @@ return {
                 section_separators = { left = '', right = '' },
                 component_separators = { left = '', right = '' }
             },
+            sections = {
+                lualine_c = {
+                    {
+                        'filename',
+                        file_status = true, 
+                        new_file_status = false,
+                        path = 1,
+                        shorting_target = 40,
+                        symbols = {
+                            modified = '●',
+                            readonly = '🔒',
+                            unnamed = '[No Name]',
+                            newfile = '[New]',
+                        }
+                    }
+                }
+            },
             extensions = { 'mason' }
         },
     },
